3b366cb1f638ab7a4039681deb491c8248a36dab
sifat786 <solaimanislamsifat@gmail.com>
2024-04-29 02:49:43 +0600

    modify vercel.json

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await craftCollection.deleteOne(query);
       res.send(result);
-    }) 
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
