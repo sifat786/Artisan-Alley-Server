97535bf4f9dcb5ed63dedaa837879bb10b4d17f9
sifat786 <solaimanislamsifat@gmail.com>
2024-04-29 01:57:13 +0600

    added put method

--- index.js
+++ index.js
@@ -29,13 +29,6 @@ async function run() {
     await client.connect();
     const craftCollection = client.db("craftDB").collection("CRAFTS");
 
-    //! POST
-    app.post('/crafts', async(req, res) => {
-      const newCrafts = req.body;
-      const result = await craftCollection.insertOne(newCrafts);
-      res.send(result);
-    })
-
     //! GET
     app.get('/crafts', async(req, res) => {
       const cursor = craftCollection.find();
@@ -55,6 +48,44 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    //! POST
+    app.post('/crafts', async(req, res) => {
+      const newCrafts = req.body;
+      const result = await craftCollection.insertOne(newCrafts);
+      res.send(result);
+    })
+
+    //! PUT
+    app.put('/crafts/:id', async(req, res) => {
+      const id = req.params.id;
+      const crafts = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true};
+      const updatedCrafts = {
+        $set: {
+            name: crafts.name,
+            price: crafts.price,
+            rating: crafts.rating,
+            time: crafts.time,
+            photo: crafts.photo,
+            category: crafts.category,
+            customization: crafts.customization,
+            stock: crafts.stock,
+            description: crafts.description,
+        }
+      }
+      const result = await craftCollection.updateOne(filter, updatedCrafts, options);
+      res.send(result);
+    })
+
+    // //! Delete
+    // app.delete('/crafts/:id', async(req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await craftCollection.deleteOne(query);
+    //   res.send(result);
+    // }) 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
